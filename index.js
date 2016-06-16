3cb0c9cae1480a9aca6ced6047303eac3b0a6542
Robert Koch <robert@mentallyfriendly.com>
2016-06-16 19:43:20 +1000

    basic version up and running

--- index.js
+++ index.js
@@ -13,3 +13,46 @@ process.env.NOW_URL
  * https://github.com/howdyai/botkit/blob/master/readme-slack.md#outgoing-webhooks-and-slash-commands
  *
  */
+
+var Botkit = require('botkit');
+var controller = Botkit.slackbot({
+  debug:true
+});
+var bot = controller.spawn({
+  token: process.env.SLACKBOT_TOKEN
+});
+bot.startRTM(function(error, whichBot, payload) {
+  if (error) {
+    throw new Error('Could not connect to Slack');
+  }
+});
+
+// controller.hears(['hello'], ['direct_message','direct_mention','mention','ambient'], function(whichBot, message) {
+//   whichBot.reply(message, 'Did you say my name?');
+// });
+
+// controller.on('ambient', function(bot, message) {
+//     bot.reply(message,{
+//       text: "I'm a hungry panda",
+//       username: "Panda Luigi",
+//       icon_emoji: ":panda_face:"
+//     });
+// });
+
+var approves = {
+  "pizza": "yes"
+};
+
+controller.hears(['hello'], ['ambient'], function(bot, message) {
+
+  bot.startConversation(message,function(err,convo) {
+
+    convo.ask('What\'s your favourite thing about' + approves.pizza, function(response,convo) {
+
+      convo.say('Cool, me to. I also like: ' + response.text);
+      convo.next();
+
+    });
+
+  });
+});
