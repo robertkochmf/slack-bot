d6560d4497c943226e87e50688acd937ac0b969d
Robert Koch <robert@mentallyfriendly.com>
2016-06-26 13:32:18 +1000

    all working so far

--- index.js
+++ index.js
@@ -14,18 +14,42 @@ process.env.NOW_URL
  *
  */
 
+var fetch = require('node-fetch');
 var Botkit = require('botkit');
-var controller = Botkit.slackbot({
-  debug:true
+var firebaseConfig = {
+    apiKey: "AIzaSyA5PHbaSMs2beKDLsUZc9nuLdqJT8QTiDw",
+    authDomain: "yasin-bot.firebaseapp.com",
+    databaseURL: "https://yasin-bot.firebaseio.com",
+    storageBucket: "",
+  };
+var firebaseStorage = require('botkit-storage-firebase')({
+  firebase_uri: 'https://yasin-bot.firebaseio.com'
 });
+var controller = Botkit.slackbot({
+  storage: firebaseStorage,
+  // debug:true
+});
+
 var bot = controller.spawn({
   token: process.env.SLACKBOT_TOKEN
 });
+
 bot.startRTM(function(error, whichBot, payload) {
   if (error) {
     throw new Error('Could not connect to Slack');
   }
 });
+
+
+//Get Names of All Users
+bot.api.users.list({},function(err,response) {
+  obj = response.members;
+  var names = Object.keys(obj).map(function (key) {
+    var val = obj[key];
+    return val.profile.real_name;
+  });
+});
+
 
 // controller.hears(['hello'], ['direct_message','direct_mention','mention','ambient'], function(whichBot, message) {
 //   whichBot.reply(message, 'Did you say my name?');
@@ -39,20 +63,169 @@ bot.startRTM(function(error, whichBot, payload) {
 //     });
 // });
 
-var approves = {
-  "pizza": "yes"
+// var approves = {
+//   pizza: "fuck yes",
+// };
+
+// Test DB
+var approveDB = {
+  id: 'approvals',
+  items: {
+  }
 };
 
-controller.hears(['hello'], ['ambient'], function(bot, message) {
+// TODO: Setup an init that creates the firebase DB
+// if (getDB() ==)
+
+function getDB () {
+  controller.storage.teams.get('approvals', function(err,user_data) {
+    approveDB.items = user_data.items;
+  });
+}
+
+// var makeDB = new Promise(function(resolve, reject) {
+//   controller.storage.teams.get('make', function(err,user_data) {
+//     return user_data;
+//   });
+// });
+
+// Name and setup database for approvals
+var dbName = 'approvals';
+var dbContent;
+
+// Check FireBase to see if database already exists
+function get(key) {
+  return new Promise(function(resolve, reject) {
+    controller.storage.teams.get(key, function(err,user_data) {
+      if (user_data !== null) {
+        resolve(user_data);
+      }else {
+        reject(Error("Collection not found"));
+      }
+    });
+  });
+}
+
+// Create new FireBase database
+function createDB(id) {
+  database = {id: id,items: {0: '0'}};
+  controller.storage.teams.save(database);
+}
+
+get(dbName).then(function(user_data) {
+  console.log("Success");
+  dbContent = user_data;
+  console.log(dbContent);
+}, function(error){
+  console.error("Failed", error);
+  console.log("Creating new database for ", dbName);
+  createDB(dbName);
+});
+
+
+
+
+
+// var makeDB = controller.storage.teams.get('make', function(err,user_data) {
+//   console.log(user_data);
+//   return user_data;
+//
+// });
+
+// if (makeDB === null) {
+//   console.log('it is null');
+// }
+
+// if user_data = null create database
+
+// console.log(makeDB);
+
+
+// console.log(approveDB);
+
+
+//
+// Get firebase DB
+// User asks Does Yasin approve of cakes
+// Bot conversation is started
+// Look inside firebase DB object
+// If item is in DB object reply with value
+// If item not in DB
+
+controller.hears('Does Yasin approve of (.*)',['ambient'], function(bot, message) {
+  getDB();
+  var item = message.match[1];
 
   bot.startConversation(message,function(err,convo) {
 
-    convo.ask('What\'s your favourite thing about' + approves.pizza, function(response,convo) {
+    if (!(approveDB.items[item])) {
 
-      convo.say('Cool, me to. I also like: ' + response.text);
-      convo.next();
+      var requestURL = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=' + item;
 
-    });
+      fetch(requestURL).then(function(response) {
+
+        return response.json();
+
+      }).then(function(json) {
+        var gifURL = json.data.image_url;
+
+        bot.reply(message, {
+          text: 'Yasin does not approve of ' + item,
+          attachments : [
+            {
+              title: 'Yasin Dissaproves',
+              image_url: gifURL
+            }
+          ]
+        });
+
+      });
+
+      setTimeout(function () {
+        convo.ask('Would you like to add ' + item + '?',[
+          {
+            pattern: bot.utterances.yes,
+            callback: function(response,convo) {
+              convo.ask('Great! What does Yasin think of ' + item + '?', function(message,convo) {
+                  // TODO: Figure out how to save this new approval out to a variable, then push that to firebase once the conversation is over
+                  approves[item] = message.text;
+                  console.log(approves);
+                  convo.say('All added!');
+                  convo.next();
+                });
+              convo.next();
+            }
+
+          },
+          {
+            pattern: bot.utterances.no,
+            callback: function(response,convo) {
+              convo.say('Ok! Well perhaps later.');
+              convo.next();
+              convo.stop();
+            }
+          },
+          {
+            default: true,
+            callback: function(response,convo) {
+              // just repeat the question
+              convo.repeat();
+              convo.next();
+            }
+          }
+        ]);
+
+      },3000);
+
+
+    } else {
+      bot.reply(message, approveDB.items[item]);
+      convo.stop();
+    }
 
   });
+
+
+
+
 });
